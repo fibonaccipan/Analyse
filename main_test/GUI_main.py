ff033473c909664af49c55a61dc745024ed038a1
18040267 <ericpan24@gmail.com>
2018-05-23 17:51:23 +0800

    normal commit

--- main_test/GUI_main.py
+++ main_test/GUI_main.py
@@ -18,19 +18,40 @@ class MainWindow(Qtqw.QMainWindow):
         self.init_ui()
 
     def init_ui(self):
-        exitAct = Qtqw.QAction(Qtqg.QIcon('../img/quit1.png'), '&退出', self)
+        exitAct = Qtqw.QAction(Qtqg.QIcon('../img/quit1.png'), '&退出', self)  # 设置退出按钮，属于文件菜单
         exitAct.setShortcut('Ctrl+Q')
         exitAct.setStatusTip('退出程序')
         exitAct.triggered.connect(self.app_quit)
+
+        iptDtAct = Qtqw.QAction(Qtqg.QIcon('../img/import.png'), '&导入数据', self)  # 设置导入按钮，属于文件菜单
+        iptDtAct.setShortcut('Ctrl+I')
+        iptDtAct.setStatusTip('导入比赛数据')
+        iptDtAct.triggered.connect(self.import_file)
+
+        delDtAct = Qtqw.QAction(Qtqg.QIcon('../img/import.png'), '&删除场次', self)  # 设置导入按钮，属于文件菜单
+        delDtAct.setShortcut('Ctrl+D')
+        delDtAct.setStatusTip('删除场次数据')
+        delDtAct.triggered.connect(self.delete_round)
+
         self.statusBar()
         menubar = self.menuBar()
         fileMenu = menubar.addMenu('&文件')
         fileMenu.addAction(exitAct)
+        fileMenu.addAction(iptDtAct)
+        fileMenu = menubar.addMenu('&编辑')
+        fileMenu.addAction(delDtAct)
 
-        self.setWindowTitle("yant狗逼的程序")
+
+        self.setWindowTitle("狗逼的程序")
         self.setGeometry(300, 300, 600, 300)
         self.setWindowIcon(Qtqg.QIcon('../img/axur17.png'))
         self.show()
+
+    def delete_round(self):
+        pass
+
+    def import_file(self):
+        pass
 
     def app_quit(self):
         replay = Qtqw.QMessageBox.question(self, "消息", "确认退出么？",
