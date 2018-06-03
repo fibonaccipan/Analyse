359998d604b650b601d8bb625999fae8d595ad78
Fibonacci <ericpan1124@yahoo.com>
2018-06-03 21:37:29 +0800

    home laptop

---
+++ edge_test/tesat_2.py
@@ -0,0 +1,26 @@
+
+class Test:
+    def __init__(self):
+        self.dict1 = {"aaa": "111", "bbb": "222"}
+        self.dict2: dict
+
+    def fun(self):
+        print(self.dict1)
+        print(self.dict1["aaa"])
+        f = open('./temp.txt','w')
+        f.write(str(self.dict1))
+        f.close()
+
+    def rd(self):
+        f = open('./temp.txt', 'r')
+        a = f.read()
+        self.dict2 = eval(a)
+        f.close()
+
+
+if __name__ == '__main__':
+    t1 = Test()
+    t1.fun()
+    t1.rd()
+    print(t1.dict2)
+    print(t1.dict2["bbb"])
