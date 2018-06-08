f65e124c22fbf6739ae4038340925132b330e46d
18040267 <ericpan24@gmail.com>
2018-06-08 10:16:22 +0800

    office commit

--- window/centralWidget.py
+++ window/centralWidget.py
@@ -10,11 +10,14 @@ Final : No
 """
 import sys
 import PyQt5.QtWidgets as Qtqw
+import PyQt5.QtCore as Qtqc
 
 
 class CentralWidget(Qtqw.QWidget):
     def __init__(self):
         super().__init__()
+        self.Qtree = self.initTree()
+        self.Qtable = self.initTable()
         self.initUI()
         # self.show()
 
@@ -24,10 +27,30 @@ class CentralWidget(Qtqw.QWidget):
 
         Hbox = Qtqw.QHBoxLayout()
         # btn = Qtqw.QPushButton("按钮", self)
+        self.setLayout(Hbox)
+        # Hbox.addStretch(1)
+        Hbox.addWidget(self.Qtree)
+        Hbox.addWidget(self.Qtable)
+        Hbox.setStretchFactor(self.Qtree, 1)
+        Hbox.setStretchFactor(self.Qtable, 4)
+
+    def initTable(self):
+        myTable = Qtqw.QTableWidget(5, 3)
+        myTable.setEditTriggers(Qtqw.QAbstractItemView.DoubleClicked)
+        myTable.verticalHeader().setVisible(False)
+        myTable.horizontalHeader().setVisible(False)
+        myTable.setSpan(0, 0, 2, 1)
+        flag = Qtqc.Qt.ItemFlags(63)
+        newItem = Qtqw.QTableWidgetItem("666")
+        newItem.setFlags(flag)
+        myTable.setItem(1, 1, newItem)
+        return myTable
+
+    def initTree(self):
         Qtree = Qtqw.QTreeWidget()
         Qtree.setHeaderHidden(True)
         root = Qtqw.QTreeWidgetItem(Qtree)
-        root.setText(0,"通用数据分析工具")
+        root.setText(0, "通用数据分析工具")
         firstLvl1 = Qtqw.QTreeWidgetItem(root)
         firstLvl1.setText(0, "创业者3.0")
         firstLvl2 = Qtqw.QTreeWidgetItem(root)
@@ -46,11 +69,7 @@ class CentralWidget(Qtqw.QWidget):
         secondLvl5.setText(0, "试题5")
         secondLvl6 = Qtqw.QTreeWidgetItem(firstLvl2)
         secondLvl6.setText(0, "试题6")
-        self.setLayout(Hbox)
-        # Hbox.addStretch(1)
-        Hbox.addWidget(Qtree)
-        Hbox.addStretch(2)
-
+        return Qtree
 
 if __name__ == '__main__':
     app = Qtqw.QApplication(sys.argv)
